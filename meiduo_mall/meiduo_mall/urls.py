3c6389c84a3159a9ac82da95d5fb7188d601c936
lishao <285051863@qq.com>
2022-05-29 16:47:57 +0800

    判断用户名是否重复

--- meiduo_mall/meiduo_mall/urls.py
+++ meiduo_mall/meiduo_mall/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from django.http import HttpResponse
 
 def log(request):
@@ -32,5 +32,6 @@ def log(request):
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('log/', log),
+    # 导入users子应用的路由
+    path('',include('apps.users.urls')),
 ]
